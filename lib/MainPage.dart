9a235c340124e161bb9d3f27c01aef733c99edb1
Furkan YILDIRIM <46926199+Furkany99@users.noreply.github.com>
2024-07-10 00:35:23 +0300

    Drawer

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -13,26 +13,55 @@ class MainPage extends StatelessWidget {
           children: <Widget>[
             DrawerHeader(
               decoration: BoxDecoration(
-                color: Colors.blue,
+                color: Colors.cyan,
               ),
               child: Text(
-                'Menu',
+                'Emsile',
                 style: TextStyle(
-                  color: Colors.white,
+                  color: Colors.grey,
                   fontSize: 24,
                 ),
               ),
             ),
             ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
+              leading: Icon(Icons.home_outlined),
+              title: Text('Ana sayfa'),
               onTap: () {
                 Navigator.pop(context);
               },
             ),
             ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
+              leading: Icon(Icons.settings_outlined),
+              title: Text('Ayarlar'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.share_outlined),
+              title: Text('Paylaş'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.error_outline),
+              title: Text('Hata bildir'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            const Divider(color: Colors.black),
+            ListTile(
+              leading: Icon(Icons.info_outline),
+              title: Text('Uygulama hakkında'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.star_rate_outlined),
+              title: Text('Uygulamayı puanla'),
               onTap: () {
                 Navigator.pop(context);
               },
